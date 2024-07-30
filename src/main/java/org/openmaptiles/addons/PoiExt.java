28f0aa0b3bc76afd2db2aa024598c8313218554e
Markus Suomi <markus.r.suomi@gmail.com>
2024-07-30 17:37:15 +0300

    Changes: - New OSM POIs & access check

--- src/main/java/org/openmaptiles/addons/PoiExt.java
+++ src/main/java/org/openmaptiles/addons/PoiExt.java
@@ -4,12 +4,17 @@ import com.onthegomap.planetiler.FeatureCollector;
 import com.onthegomap.planetiler.reader.SourceFeature;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import org.openmaptiles.Layer;
 import org.openmaptiles.OpenMapTilesProfile;
 
 public class PoiExt implements Layer, OpenMapTilesProfile.OsmAllProcessor {
 
   private static final String LAYER_NAME = "poi_ext";
+    
+  private static final Set<String> ACCESS_NO_VALUES = Set.of(
+    "private", "no", "military", "permit", "delivery", "customers"
+  );
 
   @Override
   public String name() {
@@ -56,17 +61,34 @@ public class PoiExt implements Layer, OpenMapTilesProfile.OsmAllProcessor {
       // Key is tag combination; value is "class,subclass"
       tagToClassSubclass.put("man_made=tower,tower:type=observation", "tower,observation");
       tagToClassSubclass.put("amenity=shelter,shelter_type=lean_to", "shelter,lean_to");
-       tagToClassSubclass.put("amenity=shelter,shelter_type=basic_hut", "shelter,lean_to");
+      tagToClassSubclass.put("amenity=shelter,shelter_type=basic_hut", "shelter,lean_to");
       tagToClassSubclass.put("amenity=shelter,shelter_type=picnic_shelter", "shelter,picnic_shelter");
       tagToClassSubclass.put("amenity=shelter,shelter_type=weather_shelter", "shelter,weather_shelter");
+      tagToClassSubclass.put("amenity=shelter,shelter_type=gazebo", "shelter,gazebo");
       tagToClassSubclass.put("amenity=water_point", "water,water_point");
       tagToClassSubclass.put("amenity=drinking_water", "water,drinking_water");
       tagToClassSubclass.put("amenity=toilets", "amenity,toilets");
+      tagToClassSubclass.put("amenity=bar", "amenity,bar");
+      tagToClassSubclass.put("amenity=pub", "amenity,pub");
+      tagToClassSubclass.put("amenity=cafe", "amenity,cafe");
+      tagToClassSubclass.put("amenity=fast_food", "amenity,fast_food");
+      tagToClassSubclass.put("amenity=restaurant", "amenity,restaurant");
+      tagToClassSubclass.put("amenity=bicycle_repair_station", "amenity,bicycle_repair_station");
       tagToClassSubclass.put("building=toilets", "amenity,toilets");
       tagToClassSubclass.put("tourism=viewpoint", "tourism,viewpoint");
       tagToClassSubclass.put("tourism=wilderness_hut", "tourism,wilderness_hut");
+      tagToClassSubclass.put("tourism=attraction", "tourism,attraction");
+      tagToClassSubclass.put("tourism=hotel", "tourism,hotel");
+      tagToClassSubclass.put("tourism=hostel", "tourism,hostel");
+      tagToClassSubclass.put("tourism=motel", "tourism,motel");
+      tagToClassSubclass.put("tourism=apartment", "tourism,apartment");
+      tagToClassSubclass.put("tourism=chalet", "tourism,chalet");
+      tagToClassSubclass.put("tourism=camp_pitch", "tourism,camp_pitch");
+      tagToClassSubclass.put("tourism=camp_site", "tourism,camp_site");
       tagToClassSubclass.put("leisure=swimming_area", "leisure,swimming_area");
       tagToClassSubclass.put("leisure=firepit", "leisure,firepit");
+      tagToClassSubclass.put("leisure=picnic_table", "leisure,picnic_table");
+      tagToClassSubclass.put("leisure=park", "leisure,park");
       tagToClassSubclass.put("building=bunker", "building,bunker");
       tagToClassSubclass.put("military=bunker", "military,bunker");
       tagToClassSubclass.put("historic=monument", "historic,monument");
@@ -75,6 +97,17 @@ public class PoiExt implements Layer, OpenMapTilesProfile.OsmAllProcessor {
       tagToClassSubclass.put("natural=peak", "natural,peak");
       tagToClassSubclass.put("obstacle=fallen_tree", "obstacle,fallen_tree");
       tagToClassSubclass.put("noexit=yes", "noexit,yes");
+      tagToClassSubclass.put("shop=bakery", "shop,bakery");
+      tagToClassSubclass.put("shop=coffee", "shop,coffee");
+      tagToClassSubclass.put("shop=convenience", "shop,convenience");
+      tagToClassSubclass.put("shop=department_store", "shop,department_store");
+      tagToClassSubclass.put("shop=food", "shop,food");
+      tagToClassSubclass.put("shop=general", "shop,general");
+      tagToClassSubclass.put("shop=ice_cream", "shop,ice_cream");
+      tagToClassSubclass.put("shop=mall", "shop,mall");
+      tagToClassSubclass.put("shop=pastry", "shop,pastry");
+      tagToClassSubclass.put("shop=supermarket", "shop,supermarket");
+
       return tagToClassSubclass;
   }
 
@@ -89,6 +122,12 @@ public class PoiExt implements Layer, OpenMapTilesProfile.OsmAllProcessor {
 
   private void addFeature(FeatureCollector features, String initialMethod, String clazz, String subclass,SourceFeature feature) {
     com.onthegomap.planetiler.FeatureCollector.Feature finalFeature;
+
+    Object access = feature.getTag("access");
+    if (access != null && ACCESS_NO_VALUES.contains(access)) {
+        return;
+    }
+
     if ("point".equals(initialMethod)) {
         finalFeature = features.point(LAYER_NAME);
     } else if ("centroidIfConvex".equals(initialMethod)) {
