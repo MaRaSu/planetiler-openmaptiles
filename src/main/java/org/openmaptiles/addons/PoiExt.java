25774c51e0f16c3efa2ae24def5494ba9a5a33e5
Markus Suomi <markus.r.suomi@gmail.com>
2025-04-06 10:03:45 +0300

    Remove place=island from PoiExt since it failed the test

--- src/main/java/org/openmaptiles/addons/PoiExt.java
+++ src/main/java/org/openmaptiles/addons/PoiExt.java
@@ -112,7 +112,6 @@ public class PoiExt implements Layer, OpenMapTilesProfile.OsmAllProcessor {
     tagToClassSubclass.put("ford=yes", "ford,yes");
     tagToClassSubclass.put("ford=no", "ford,no");
     tagToClassSubclass.put("place=square", "place,square");
-    tagToClassSubclass.put("place=island", "place,island");
     tagToClassSubclass.put("place=archipelago", "place,archipelago");
     tagToClassSubclass.put("natural=peninsula", "natural,peninsula");
     tagToClassSubclass.put("natural=isthmus", "natural,isthmus");
